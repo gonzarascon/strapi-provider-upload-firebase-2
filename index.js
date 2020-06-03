673b2fe515ec6378bb9fd28994ea1a5788e1a431
gonzarascon <gonzarascon@gmail.com>
2020-06-03 17:18:58 -0300

    Stable upload function

--- index.js
+++ index.js
@@ -4,7 +4,9 @@
  * Module dependencies
  */
 
-const admin = require("firebase-admin");
+const admin = require("firebase-admin"),
+  fs = require("fs"),
+  path = require("path");
 let bucket = undefined;
 
 module.exports = {
@@ -18,24 +20,31 @@ module.exports = {
     }
 
     return {
-      upload: async (file) => {
-        console.log("UPLOADED FILE", file);
-        const path = file.path
-          ? `${file.path}/`
-          : `/uploads/${file.hash}${file.ext}`;
-        try {
-          const [firestoreFile] = await bucket.upload(path, {
-            destination: `${file.hash}${file.ext}`,
-            contentType: file.mime,
+      upload: (file) => {
+        const base_temp_url = path.join(
+          strapi.config.paths.static,
+          `/uploads/temp_${file.hash}${file.ext}`
+        );
+
+        return new Promise((resolve, reject) => {
+          fs.writeFile(base_temp_url, file.buffer, async (err) => {
+            if (err) reject(err);
+            try {
+              const [firestoreFile] = await bucket.upload(base_temp_url, {
+                destination: `${file.hash}${file.ext}`,
+                contentType: file.mime,
+              });
+              const [url] = await firestoreFile.getSignedUrl({
+                action: "read",
+                expires: "03-01-2500",
+              });
+
+              resolve(Object.assign(file, { url }));
+            } catch (error) {
+              console.log(`Upload failed, try again: ${error}`);
+            }
           });
-          const [url] = await firestoreFile.getSignedUrl({
-            action: "read",
-            expires: "03-01-2500",
-          });
-          file.url = url;
-        } catch (error) {
-          console.log(`Upload failed, try again: ${error}`);
-        }
+        });
       },
       delete: async (file) => {
         const filename = `${file.hash}${file.ext}`;
